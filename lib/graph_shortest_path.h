6b5089e4e6bb6bbcefdf4c99fcf2e1053e93204f
jiahao.xu <jiahao.xu@shopee.com>
2021-10-17 11:42:26 +0800

    second shortest path

--- lib/graph_shortest_path.h
+++ lib/graph_shortest_path.h
@@ -7,12 +7,10 @@
 namespace contest {
 
 template <typename weight_t>
-void dijstra(
-    std::vector<std::vector<pair<int, weight_t> > >& G,
-    std::vector<weight_t>& dist,  // distance to start node, ALL elements must
-                                  // be initialed as infinity
-    vector<int>& trace,           // next node to root
-    int start                     // start node
+void dijstra(std::vector<std::vector<pair<int, weight_t> > >& G,
+             std::vector<weight_t>& dist,  // all elements must be initialed as infinity
+             vector<int>& trace,           // next node to root
+             int start                     // start
 ) {
     typedef std::tuple<weight_t, int, int> S;  // <dist, from, current>
     std::priority_queue<S, vector<S>, greater<S> > Q;
@@ -35,8 +33,48 @@ void dijstra(
 }
 
 template <typename weight_t>
+void dijstra_second(std::vector<std::vector<pair<int, weight_t> > >& G,
+              std::vector<weight_t>& dist1,  // shortest path, all init as INF!
+              std::vector<weight_t>& dist2,  // second shortest path, all init as INF!
+              int start                      // start node
+) {
+    typedef std::tuple<weight_t, int, int> S;
+    std::priority_queue<S, vector<S>, greater<S> > Q;
+    Q.push({(weight_t)(0), -1, start});
+    dist1[0] = 0;
+
+    while (Q.size()) {
+        weight_t d;
+        int from, curr;
+        tie(d, from, curr) = Q.top();
+        Q.pop();
+
+        if (dist2[curr] < d) continue;
+        for (auto& p : G[curr]) {
+            int nn = p.first, d2 = d + p.second;
+            if (dist1[nn] > d2) {
+                swap(dist1[nn], d2);
+                Q.push({dist1[nn], curr, nn});
+            }
+            // this algorithm will ignore all path with same length as shortest path
+            // for example, for some path [2, 2, 4], it return 4
+            // if you want it return the second shortest path with different edge
+            // but can share the same length, change "dist[nn] < d2" as "dist[nn] <= d2"
+            // or just remove it
+            if (dist2[nn] > d2 and dist1[nn] < d2) {
+                dist2[nn] = d2;
+                Q.push({dist2[nn], curr, nn});
+            }
+        }
+    }
+}
+
+template <typename weight_t>
 bool bellman_ford(std::vector<std::vector<pair<int, weight_t> > >& G,
-                  std::vector<weight_t>& dist, vector<int>& trace, int start) {
+                  std::vector<weight_t>& dist,  // all elements must be initialed as infinity
+                  vector<int>& trace,           // next node to root
+                  int start                     // start
+) {
     int n = G.size();
     std::queue<std::tuple<weight_t, int, int> > Q;  // <dist, from, current>
     Q.push({(weight_t)(0), -1, start});
