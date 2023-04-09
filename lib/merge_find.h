d57dd57927ab1d15a7c9229700112cc07f1cca1a
jiahao.xu <jiahao.xu@shopee.com>
2023-04-09 14:03:06 +0800

    merge-find set as array remover

--- lib/merge_find.h
+++ lib/merge_find.h
@@ -5,11 +5,22 @@ namespace contest {
 
 struct MergeFindSet {
     std::vector<int> p;
-    MergeFindSet(int n) : p(n) { init(); }
     int find(int x) { return p[x] == x ? x : p[x] = find(p[x]); }
     void merge(int root, int child) { p[find(child)] = find(root); }
-    void init() {
-        for (int i = 0; i < int(p.size()); i++) p[i] = i;
+    MergeFindSet(int n) : p(n) {
+        for (int i = 0; i < n; i++) p[i] = i;
+    }
+};
+
+struct MergeBigger {
+    std::vector<int> p;
+    int find(int x) { return p[x] == x ? x : p[x] = find(p[x]); }
+    void merge(int x, int y) {
+        int rx = find(x), ry = find(y);
+        p[std::min(rx, ry)] = std::max(rx, ry);
+    }
+    MergeBigger(int n) : p(n) {
+        for (int i = 0; i < n; i++) p[i] = i;
     }
 };
 
