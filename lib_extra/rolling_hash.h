2ee33c942f66d9133733d891cf5d41a4048f0212
plus2047 <0x00002012@gmail.com>
2024-09-16 11:22:01 +0800

    rolling

--- lib_extra/rolling_hash.h
+++ lib_extra/rolling_hash.h
@@ -1,76 +1,84 @@
 #pragma once
 #include <vector>
+#include <array>
 
 // https://www.geeksforgeeks.org/string-hashing-using-polynomial-rolling-hash-function/
 
 namespace contest {
 
-long long power(long long x, long long y, long long p) {
-    long long result = 1;
-    for (; y; y >>= 1, x = x * x % p) {
-        if (y & 1) {
-            result = result * x % p;
+struct Rolling {
+    const static int hash_size = 2;
+    typedef array<int, hash_size> Hash;
+
+    // mod * mod < INT64_MAX
+    const static int mod = 1e9 + 7;
+    constexpr static Hash primes = {19, 23};
+    constexpr static Hash zeros = {0, 0};
+    constexpr static Hash ones = {1, 1};
+    inline static std::vector<Hash> inv = {};
+
+    int size = 0;
+    std::vector<Hash> hash;
+
+    Rolling(const std::string& s): size(s.size()), hash(s.size()) {
+        if(size > 0) {
+            init(s);
         }
     }
-    return result;
-}
 
-long long inverse(long long x, long long p) {
-    return power(x, p - 2, p);
-}
-
-struct RollingHash {
-    const int mod1 = 1e9 + 7, mod2 = 1e9 + 9;
-    const int p1 = 31, p2 = 37;
-
-    std::vector<int> hash1, hash2;
-    std::vector<int> inv_pow1, inv_pow2;
-    int length = 0, inv_size = 1;
-
-    RollingHash(const std::string& s) {
-        length = s.size();
-        hash1.resize(length);
-        hash2.resize(length);
-
-        int h1 = 0, h2 = 0;
-        long long p_pow1 = 1, p_pow2 = 1;
-        for (int i = 0; i < length; i++) {
-            h1 = (h1 + s[i] * p_pow1) % mod1;
-            h2 = (h2 + s[i] * p_pow2) % mod2;
-            p_pow1 = (p_pow1 * p1) % mod1;
-            p_pow2 = (p_pow2 * p2) % mod2;
-            hash1[i] = h1;
-            hash2[i] = h2;
+    void init(const std::string& s) {
+        size = s.size();
+        hash.resize(size);
+        Hash h = zeros, p_pow = ones;
+        for (int i = 0; i < size; i++) {
+            for(int j = 0; j < hash_size; j++) {
+                h[j] = (h[j] + 1LL * s[i] * p_pow[j]) % mod;
+                p_pow[j] = (1LL * p_pow[j] * primes[j]) % mod;
+            }
+            hash[i] = h;
         }
 
-        if (inv_size < length) {
-            for (; inv_size < length; inv_size <<= 1)
-                ;
+        if(int(inv.size()) < size) {
+            int prev_size = inv.size();
+            inv.resize(size);
 
-            inv_pow1.resize(inv_size, -1);
-            inv_pow2.resize(inv_size, -1);
+            for(int j = 0; j < hash_size; j++) {
+                inv[size - 1][j] = power(primes[j], mod - size, mod);
+            }
 
-            inv_pow1[inv_size - 1] = inverse(power(p1, inv_size - 1, mod1), mod1);
-            inv_pow2[inv_size - 1] = inverse(power(p2, inv_size - 1, mod2), mod2);
-
-            for (int i = inv_size - 2; i >= 0 && inv_pow1[i] == -1; i--) {
-                inv_pow1[i] = (1LL * inv_pow1[i + 1] * p1) % mod1;
-                inv_pow2[i] = (1LL * inv_pow2[i + 1] * p2) % mod2;
+            for (int i = size - 2; i >= prev_size; i--) {
+                for(int j = 0; j < hash_size; j++) {
+                    inv[i][j] = (1LL * inv[i + 1][j] * primes[j]) % mod;
+                }
             }
         }
     }
 
-    std::pair<int, int> substr(const int l, const int r) {
+    Hash substr(const int l, const int r) {
         if (l == 0) {
-            return {hash1[r], hash2[r]};
+            return hash[r];
         }
-        int temp1 = hash1[r] - hash1[l - 1];
-        int temp2 = hash2[r] - hash2[l - 1];
-        temp1 += (temp1 < 0 ? mod1 : 0);
-        temp2 += (temp2 < 0 ? mod2 : 0);
-        temp1 = (temp1 * 1LL * inv_pow1[l]) % mod1;
-        temp2 = (temp2 * 1LL * inv_pow2[l]) % mod2;
-        return {temp1, temp2};
+        Hash h;
+        for(int i = 0; i < hash_size; i++) {
+            h[i] = hash[r][i] - hash[l - 1][i];
+            h[i] += h[i] < 0 ? mod : 0;
+            h[i] = (1LL * h[i] * this->inv[l][i]) % mod;
+        }
+        return h;
+    }
+
+    static long long power(long long x, long long y, long long p) {
+        long long result = 1;
+        for (; y; y >>= 1, x = x * x % p) {
+            if (y & 1) {
+                result = result * x % p;
+            }
+        }
+        return result;
+    }
+
+    static long long inverse(long long x, long long p) {
+        return power(x, p - 2, p);
     }
 };
 
