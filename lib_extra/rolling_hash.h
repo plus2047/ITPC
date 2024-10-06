104f9681386715e4b2fd0fe48203f582d297a30f
plus2047 <0x00002012@gmail.com>
2024-10-06 11:53:57 +0800

    rolling hash hash

--- lib_extra/rolling_hash.h
+++ lib_extra/rolling_hash.h
@@ -82,4 +82,15 @@ struct Rolling {
     }
 };
 
+template<>
+struct std::hash<Rolling::Hash>
+{
+    std::size_t operator()(const Rolling::Hash& h) const noexcept
+    {
+        std::size_t h1 = std::hash<int>{}(h[0]);
+        std::size_t h2 = std::hash<int>{}(h[1]);
+        // ...
+        return h1 ^ (h2 << 1); // or use boost::hash_combine
+    }
+};
 }  // namespace contest
